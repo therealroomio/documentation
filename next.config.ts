deb7b4f4f6bd465f6f24e232247e145837f772c4
Roomio <info@valeofx.com>
2024-12-18 21:58:03 -0500

    Revert "Remove Next.js configuration file `next.config.ts` as part of project refactoring"
    This reverts commit dc6eabce537cc2e6e591cc42ebd206e54fd4433b.

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   output: 'standalone',
   reactStrictMode: true,
   images: {
@@ -8,4 +9,4 @@ const nextConfig = {
   }
 };
 
-module.exports = nextConfig; 
+export default nextConfig;
